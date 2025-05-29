49d2f4d5b2a19274cd4e0c5bd4e2a266c3a3e68e
PedroRBrito <pedrorocha1654@gmail.com>
2025-05-28 22:12:00 -0300

    Desenvolvendo construção do pdf na classe principal

--- src/main_window.py
+++ src/main_window.py
@@ -1,12 +1,17 @@
+from typing import Optional
 from PySide2.QtWidgets import QMainWindow, QFileDialog
 from interface import Ui_MainWindow
+from excel_utils import ExcelUtils
+from pdf_utils import PDFUtils
 import excel_utils, pdf_utils, email_utils
+import os
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.excel_file: Optional[str] = None
     
         # Conectar botões a funções
         self.ui.pushButton_historico.clicked.connect(self.historico)
@@ -16,22 +21,28 @@ class MainWindow(QMainWindow):
         self.ui.pushButton_resetar.clicked.connect(self.resetar_opcoes)
 
 
-    def carregar_arquivo_excel(self):
+    def carregar_arquivo_excel(self) -> None:
         options = QFileDialog.Options()
         file_name, _ = QFileDialog.getOpenFileName(self, "Carregar arquivo Excel", "", "Excel Files (*.xlsx);;All Files (*)", options=options)
         if file_name:
-            self.ui.label_nome_arquivo.setText(file_name)
+            self.nome_arquivo_abv = os.path.basename(file_name)
+            self.ui.label_nome_arquivo.setText(self.nome_arquivo_abv)
             self.excel_file = file_name
-            self.process_excel_file(file_name)
+
 
     def resetar_opcoes(self):
-        pass
+        self.ui.label_nome_arquivo.setText("1. Selecione o arquivo Excel para processar:")
+        
 
     def historico(self):
         pass
 
-    def fechar(self):
+    def enviar_email(self):
+        if not self.excel_file:
+            print("Nenhum arquivo excel selecionado")
+        df = ExcelUtils.ler_excel(self.excel_file)
+        
         pass
 
-    def enviar_email(self):
-        pass
+    def fechar(self):
+        self.close()
